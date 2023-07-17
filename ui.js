1a95fbd6e1e5b62bfab90c82f3f660cf18c77f4b
Anandita Singh <97512151+ananditasinghh@users.noreply.github.com>
2023-07-18 05:03:10 +0530

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,10 +1,324 @@
 
   
+// Define an object with the plant data
 const plantData = {
-    "Rose": {"name": "Rose", "species": "Rosa", "wateringFrequency": 2, "sunlightRequirement": 8, "growthStage": "Young"},
-    "Lily": {"name": "Lily", "species": "Lilium", "wateringFrequency": 3, "sunlightRequirement": 6, "growthStage": "Growing"},
-    "Sunflower": {"name": "Sunflower", "species": "Helianthus", "wateringFrequency": 1, "sunlightRequirement": 7, "growthStage": "Mature"}
+    "Rose": {
+        "name": "Rose",
+        "species": "Rosa",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 8,
+        "growthStage": "Young"
+    },
+    "Lily": {
+        "name": "Lily",
+        "species": "Lilium",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 6,
+        "growthStage": "Growing"
+    },
+    "Sunflower": {
+        "name": "Sunflower",
+        "species": "Helianthus",
+        "wateringFrequency": 1,
+        "sunlightRequirement": 7,
+        "growthStage": "Mature"
+    },
+    "Orchid": {
+        "name": "Orchid",
+        "species": "Orchidaceae",
+        "wateringFrequency": 5,
+        "sunlightRequirement": 5,
+        "growthStage": "Young"
+    },
+    "Fern": {
+        "name": "Fern",
+        "species": "Polypodiopsida",
+        "wateringFrequency": 4,
+        "sunlightRequirement": 4,
+        "growthStage": "Growing"
+    },
+    "Tulip": {
+        "name": "Tulip",
+        "species": "Tulipa",
+        "wateringFrequency": 4,
+        "sunlightRequirement": 6,
+        "growthStage": "Growing"
+    },
+    "Daisy": {
+        "name": "Daisy",
+        "species": "Bellis perennis",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 5,
+        "growthStage": "Young"
+    },
+    "Lavender": {
+        "name": "Lavender",
+        "species": "Lavandula",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 7,
+        "growthStage": "Mature"
+    },
+    "Cactus": {
+        "name": "Cactus",
+        "species": "Cactaceae",
+        "wateringFrequency": 5,
+        "sunlightRequirement": 8,
+        "growthStage": "Young"
+    },
+    "Ivy": {
+        "name": "Ivy",
+        "species": "Hedera",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 4,
+        "growthStage": "Growing"
+    },
+    "Begonia": {
+        "name": "Begonia",
+        "species": "Begonia",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    },
+    "Ficus": {
+        "name": "Ficus",
+        "species": "Ficus",
+        "wateringFrequency": 4,
+        "sunlightRequirement": 6,
+        "growthStage": "Young"
+    },
+    "Snake Plant": {
+        "name": "Snake Plant",
+        "species": "Sansevieria",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 4,
+        "growthStage": "Mature"
+    },
+    "Peace Lily": {
+        "name": "Peace Lily",
+        "species": "Spathiphyllum",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 5,
+        "growthStage": "Young"
+    },
+    "ZZ Plant": {
+        "name": "ZZ Plant",
+        "species": "Zamioculcas zamiifolia",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 6,
+        "growthStage": "Growing"
+    },
+    "Rosemary": {
+        "name": "Rosemary",
+        "species": "Rosmarinus officinalis",
+        "wateringFrequency": 4,
+        "sunlightRequirement": 6,
+        "growthStage": "Mature"
+    },
+    "Peony": {
+        "name": "Peony",
+        "species": "Paeonia",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    },
+    "Marigold": {
+        "name": "Marigold",
+        "species": "Tagetes",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 5,
+        "growthStage": "Young"
+    },
+    "Basil": {
+        "name": "Basil",
+        "species": "Ocimum basilicum",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 6,
+        "growthStage": "Mature"
+    },
+    "Hibiscus": {
+        "name": "Hibiscus",
+        "species": "Hibiscus",
+        "wateringFrequency": 5,
+        "sunlightRequirement": 7,
+        "growthStage": "Growing"
+    },
+    "Succulent": {
+        "name": "Succulent",
+        "species": "Succulentus",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 4,
+        "growthStage": "Mature"
+    },
+    "Azalea": {
+        "name": "Azalea",
+        "species": "Rhododendron",
+        "wateringFrequency": 4,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    },
+    "Jasmine": {
+        "name": "Jasmine",
+        "species": "Jasminum",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 6,
+        "growthStage": "Young"
+    },
+    "Croton": {
+        "name": "Croton",
+        "species": "Codiaeum",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    },
+    "Daffodil": {
+        "name": "Daffodil",
+        "species": "Narcissus",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 6,
+        "growthStage": "Young"
+    },
+    "Zinnia": {
+        "name": "Zinnia",
+        "species": "Zinnia",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    },
+    "Morning Glory": {
+        "name": "Morning Glory",
+        "species": "Ipomoea",
+        "wateringFrequency": 1,
+        "sunlightRequirement": 6,
+        "growthStage": "Young"
+    },
+    "Carnation": {
+        "name": "Carnation",
+        "species": "Dianthus",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    },
+    "Peperomia": {
+        "name": "Peperomia",
+        "species": "Peperomia",
+        "wateringFrequency": 4,
+        "sunlightRequirement": 4,
+        "growthStage": "Mature"
+    },
+    "Bird of Paradise": {
+        "name": "Bird of Paradise",
+        "species": "Strelitzia reginae",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 6,
+        "growthStage": "Young"
+    },
+    "Maidenhair Fern": {
+        "name": "Maidenhair Fern",
+        "species": "Adiantum",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 4,
+        "growthStage": "Growing"
+    },
+    "Spider Plant": {
+        "name": "Spider Plant",
+        "species": "Chlorophytum comosum",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 6,
+        "growthStage": "Young"
+    },
+    "Rubber Plant": {
+        "name": "Rubber Plant",
+        "species": "Ficus elastica",
+        "wateringFrequency": 4,
+        "sunlightRequirement": 6,
+        "growthStage": "Mature"
+    },
+    "Money Tree": {
+        "name": "Money Tree",
+        "species": "Pachira aquatica",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    },
+    "Aloe Vera": {
+        "name": "Aloe Vera",
+        "species": "Aloe vera",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 5,
+        "growthStage": "Young"
+    },
+    "Pothos": {
+        "name": "Pothos",
+        "species": "Epipremnum aureum",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    },
+    "Chinese Evergreen": {
+        "name": "Chinese Evergreen",
+        "species": "Aglaonema",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    },
+    "Snake Plant (Cylindrica)": {
+        "name": "Snake Plant (Cylindrica)",
+        "species": "Sansevieria cylindrica",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 4,
+        "growthStage": "Mature"
+    },
+    "Calathea": {
+        "name": "Calathea",
+        "species": "Calathea",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 5,
+        "growthStage": "Young"
+    },
+    "Hoya": {
+        "name": "Hoya",
+        "species": "Hoya",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 4,
+        "growthStage": "Mature"
+    },
+    "Fittonia": {
+        "name": "Fittonia",
+        "species": "Fittonia",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    },
+    "Moth Orchid": {
+        "name": "Moth Orchid",
+        "species": "Phalaenopsis",
+        "wateringFrequency": 5,
+        "sunlightRequirement": 6,
+        "growthStage": "Young"
+    },
+    "String of Pearls": {
+        "name": "String of Pearls",
+        "species": "Senecio rowleyanus",
+        "wateringFrequency": 2,
+        "sunlightRequirement": 6,
+        "growthStage": "Growing"
+    },
+    "Boston Fern": {
+        "name": "Boston Fern",
+        "species": "Nephrolepis exaltata",
+        "wateringFrequency": 3,
+        "sunlightRequirement": 4,
+        "growthStage": "Young"
+    },
+    "Bromeliad": {
+        "name": "Bromeliad",
+        "species": "Bromeliaceae",
+        "wateringFrequency": 4,
+        "sunlightRequirement": 5,
+        "growthStage": "Growing"
+    }
 };
+
 
 // Get references to the form and result container
 const form = document.getElementById('plant-form');
